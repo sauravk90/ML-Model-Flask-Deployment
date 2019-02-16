a574556f86fb880265bcf7b36fd060774ecee41b
Saurav.Kumar <saurav.kumar90@hotmail.com>
2019-02-16 23:56:26 +0530

    Added GUI form

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/api',methods=['POST'])
-def hello():
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
     output = prediction[0]
-    return jsonify(output)
+
+    return render_template('results.html', prediction=output)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # dic = {'experience':10, 'test_score':5, 'interview_score':6}
-    # x = np.array(list(dic.values()))
-    # print(x)
-    # print(type(x))
